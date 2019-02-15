9da5fd7475980fcca5352b2899647432a42d9b2d
shuqizhushuqi <43326013+shuqizhushuqi@users.noreply.github.com>
2019-02-14 22:51:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+    return this.questions[this.currentQuestionIndex];
+};
+
+Quiz.prototype.hasEnded = function() {
+    return this.currentQuestionIndex >= this.questions.length;
+};
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+};
+var QuizUI = {
+    displayNext: function () {
+        if (quiz.hasEnded()) {
+            this.displayScore();
+        } else {
+            this.displayQuestion();
+            this.displayChoices();
+            this.displayProgress();
+        }
+    },
+    displayQuestion: function() {
+        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    },
+    displayChoices: function() {
+        var choices = quiz.getCurrentQuestion().choices;
+
+        for(var i = 0; i < choices.length; i++) {
+            this.populateIdWithHTML("choice" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i]);
+        }
+    },
+    displayScore: function() {
+        var gameOverHTML = "<h1>Game Over</h1>";
+        gameOverHTML += "<h2> Your score is: " + quiz.score + "</h2>";
+        this.populateIdWithHTML("quiz", gameOverHTML);
+    },
+
+    populateIdWithHTML: function(id, text) {
+        var element = document.getElementById(id);
+        element.innerHTML = text;
+    },
+    guessHandler: function(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+            quiz.guess(guess);
+            QuizUI.displayNext();
+        }
+    },
+
+    displayProgress: function() {
+        var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+        this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+    }
+};
+//Create Questions
+var questions = [
+    new Question("Which one is the famous dessert in France?", [ "Chocolate Mousse", "Titamisu", "Green Tea Mochi", "I don't know" ], "Chocolate Mousse"),
+    new Question("Which one is the fake Chinese dish?", ["Egg Fried Rice","Orange Chicken", "Sticky Rice Wrap", "I don't know"], "Orange Chicken"),
+    new Question("Which one is the typical Spainish dish?", ["Gazpacho","Ratatouille", "Coq au Vin", "No idea"], "Gazpacho"),
+    new Question("Which one is the cheapest dish in general?", ["Paella","Taco", "Churro", "No idea"], "Taco"),
+    new Question("Are you hungry right now?", ["Yes","No", "Maybe", "It's okay"], "Yes")
+];
+
+//Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quiz
+QuizUI.displayNext();
